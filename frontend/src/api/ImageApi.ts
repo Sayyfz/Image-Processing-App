b854db7d0ed888751a7963178ca7121bd73fccdb
Seif Ashraf <seif_fire2@hotmail.com>
2023-01-20 13:58:05 +0200

    removed debug statements

--- frontend/src/api/ImageApi.ts
+++ frontend/src/api/ImageApi.ts
@@ -2,11 +2,6 @@ import { fromByteArray } from 'base64-js';
 import axios, { Axios, AxiosError } from 'axios';
 const BASE_URL = 'http://localhost:5000';
 
-//TODO: need to return the error string and throw the error so the try catch statement on the Home page catch and display it
-
-// const res = await axios.get(`${BASE_URL}/api/images?${queryString}`);
-// const base64Image = fromByteArray(res.data.img.data);
-// return `data:image/jpeg;base64, ${base64Image}`;
 export const resizeImg = async (queryString: string) => {
     try {
         const res = await fetch(`${BASE_URL}/api/images?${queryString}`, {
@@ -15,7 +10,6 @@ export const resizeImg = async (queryString: string) => {
 
         if (!res.ok) {
             throw new Error();
-            // console.log(res.body);
         }
         console.log(res);
 
